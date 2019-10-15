5c41bb1aea6c83740195f227cd0fd2c360af2e00
stevington <s.mcguire@hud.ac.uk>
2019-10-15 11:17:07 +0100

    Add files to commit

---
+++ Practicals/Steve/02/weather_station_V2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    capture and calculate temperature.
+    TODO
+    1, Print welcome message
+    2, ask for 6 temperatures and store the results
+    3, process the results; min, max, avg
+    4, Print the results
+"""
+
+__author__ = "Steve McGuire"
+__email__ = "s.mcguire@hud.ac.uk"
+__licence__ = "The Unlicense"
+__version__ = "2019-15-10"
+

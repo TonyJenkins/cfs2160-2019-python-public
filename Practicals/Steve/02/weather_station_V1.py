395900515fd51ce32980c2a2c44560a392a44a25
scomsdm2 <s.mcguire@hud.ac.uk>
2019-10-18 12:11:05 +0100

    Add files to commit

--- Practicals/Steve/02/weather_station_V1.py
+++ Practicals/Steve/02/weather_station_V1.py
@@ -13,4 +13,26 @@ __author__ = "Steve McGuire"
 __email__ = "s.mcguire@hud.ac.uk"
 __licence__ = "The Unlicense"
 __version__ = "2019-15-10"
+temp_list = []
+print("Welcome to the weather station")
+
+try:
+    for temp in range(6):
+        temp_list.append(float(input("Please enter temp" + str(temp + 1))))
+
+    max = max(temp_list)
+    min = min(temp_list)
+    avg = sum(temp_list) / len(temp_list)
+    print("max temp is", max)
+    print("Min temp is", min)
+    print("Avg temp is", round(avg, 2))
+except ValueError:
+    print("Numbers only")
+
+
+
+
+
+
+
 
